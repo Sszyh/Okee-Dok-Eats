bdc46e64533d260d81e99281799b348eb15923c7
Matthew Davis <matc.davis@gmail.com>
2022-10-12 21:30:02 +0000

    set up file from pair programming with gord

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,22 @@
 // Client facing scripts here
+$(() => {
+  // INDEX PAGE START
+
+  //redirect like behavior to MEME page
+    $('#or-not').on('click', () => {
+      window.location.replace("https://media.tenor.com/m0rkjXQvKYYAAAAd/im-not-hungry-upset.gif");
+    });
+
+    /*
+    $('#order-now').on('click', () => {
+      TODO, auth check, and redirect to restaurant page
+    });
+   */
+
+    $('#order-now').on('click', () => {
+
+    });
+  //INDEX PAGE END
+  });
+
+
