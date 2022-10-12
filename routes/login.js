6ee179cd8a411b6c13efa4dd4cd3f8e30595fe72
Matthew Davis <matc.davis@gmail.com>
2022-10-12 23:10:22 +0000

    add cookie-parser package, start working on automatic login route

---
+++ routes/login.js
@@ -0,0 +1,11 @@
+const users = require('./_testUsers.js')
+
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/:id', (req, res) => {
+  console.log(req.params.id)
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
-
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
@@ -25,6 +25,7 @@ app.use(
   })
 );
 app.use(express.static('public'));
+app.use(cookieParser())
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -32,6 +33,7 @@ const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 const customerRoutes = require('./routes/customer-view')
+// const loginRoutes = require('./routes/login')
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -40,6 +42,7 @@ app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 app.use('/customer', customerRoutes)
+// app.use('/login', loginRoutes)
 // Note: mount other resources here, using the same pattern above
 
 // Home page
