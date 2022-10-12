d2f3b6ad1975b172105fe76d2926bc3234f3088c
Gord Letkeman <rndedg@gmail.com>
2022-10-12 18:53:34 +0000

    Resolved conflict

--- db/schema/02_schema.sql
+++ db/schema/02_schema.sql
@@ -28,18 +28,6 @@ CREATE TABLE restaurants (
   rating SMALLINT
 );
 
-<<<<<<< HEAD
-=======
-CREATE TABLE menu_items (
-  id SERIAL PRIMARY KEY NOT NULL,
-  menu_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
-  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
-  item TEXT,
-  price MONEY,
-  time_to_prepare INT
-);
-
->>>>>>> 8fb9571fc8d466b6a1504e440a5adbd998768fe8
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
