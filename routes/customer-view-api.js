e6652bf6574224d753c1904bdfe66d8ee88018c8
Matthew Davis <matc.davis@gmail.com>
2022-10-13 21:52:24 +0000

    add comments to explain setting up separate js files for queries to the db as discussed with sarah

--- routes/customer-view-api.js
+++ routes/customer-view-api.js
@@ -10,6 +10,17 @@ router.get('/', (req, res) => {
       const restaurants = data.rows;
       res.json({ restaurants });
     })
+
+/*
+we eventually will have separate js files set up inside the queries folder with functions that will query the db for us
+
+const restaurantQueries = require('./db/queries/getRestaurants)
+    restaurantQueries.getRestaurants()
+      .then((restaurants) => {
+        res.json({restaurants})
+      })
+
+*/
     .catch(err => {
       res
         .status(500)
