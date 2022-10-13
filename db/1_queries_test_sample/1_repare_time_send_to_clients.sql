04cfece0717371a20a6b0b80e722d482764bbb38
Sszyh <zhuyer@outlook.com>
2022-10-12 21:51:45 -0700

    add queires to test the database

---
+++ db/1_queries_test_sample/1_repare_time_send_to_clients.sql
@@ -0,0 +1,4 @@
+SELECT sum(time_to_prepare) as time, orders_menu_items.order_id FROM menu_items
+JOIN orders_menu_items ON menu_item_id=menu_items.id
+GROUP BY orders_menu_items.order_id
+ORDER BY orders_menu_items.order_id;
