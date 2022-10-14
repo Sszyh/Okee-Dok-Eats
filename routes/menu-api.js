211d5c7117a3bff6df0d27a98469d784f1661b85
Matthew Davis <matc.davis@gmail.com>
2022-10-14 16:34:31 +0000

    add sql params to menu-api query

--- routes/menu-api.js
+++ routes/menu-api.js
@@ -5,14 +5,16 @@ const db = require('../db/connection');
 router.get('/', (req, res) => {
   //somehow get the params here as a string which would be the name of the restaurant we need to display the menu from, this needs to be sent as a query
   const restaurantId = req.query.id;
-  const sqlParams = ['restaurantId']
-  const query = `SELECT * FROM menu_items
-  WHERE menu_id = 1;`;
-  console.log(query);
-  db.query(query)
+  const sqlParams = [`${restaurantId}`]
+  const query = `
+  SELECT * FROM menu_items
+  WHERE menu_id = $1;
+  `;
+  console.log(query, sqlParams);
+  db.query(query, sqlParams)
     .then(data => {
-      const menu = data.rows;
-      res.json({ menu });
+      const menuList = data.rows;
+      res.json({ menuList });
     })
     .catch(err => {
       res
