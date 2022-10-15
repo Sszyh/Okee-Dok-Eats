47f5cfb021aedef5a9a969b05c999495898f844a
Sszyh <zhuyer@outlook.com>
2022-10-15 16:34:38 -0700

    restore login route

--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/:id', (req, res) => {
-  console.log("req",req.params)
   const id = req.params.id
-  console.log("id",id);
   res.cookie('user_id', id)
   res.redirect('/');
 })
