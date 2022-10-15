f3e4436b9df415c78b978f35df57d1ced0a1071b
Sszyh <zhuyer@outlook.com>
2022-10-14 17:33:12 -0700

    fix wrong conflicts

--- routes/customer-view-api.js
+++ routes/customer-view-api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router  = express.Router();
-const customerQueries = require('../db/queries/restaurant');
+
+const db = require('../db/connection');
+
 router.get('/', (req, res) => {
   const query = `SELECT * FROM restaurants`;
   console.log(query);
@@ -9,34 +11,29 @@ router.get('/', (req, res) => {
       const restaurants = data.rows;
       res.json({ restaurants });
     })
-
-/*
-we eventually will have separate js files set up inside the queries folder with functions that will query the db for us
-
-const restaurantQueries = require('./db/queries/getRestaurants)
-    restaurantQueries.getRestaurants()
-      .then((restaurants) => {
-        res.json({restaurants})
-      })
-
-<<<<<<< HEAD
-    .then(data => {
-      res.json({ restaurants: data });
-    })
-=======
-const restaurantQueries = require('../db/queries/getRestaurants)
-    restaurantQueries.getRestaurants()
-      .then((restaurants) => {
-        res.json({restaurants})
-      })
-
-*/
->>>>>>> master
     .catch(err => {
       res
         .status(500)
         .json({ error: err.message });
     });
 });
-
 module.exports = router;
+
+/*
+  following code is for connecting db/queries/
+*/
+// const customerQueries = require('../db/queries/restaurant-list');
+// router.get('/', (req, res) => {
+
+//   customerQueries.getRestaurants()
+//     .then(data => {
+//       res.json({ restaurants: data });
+//     })
+//     .catch(err => {
+//       res
+//         .status(500)
+//         .json({ error: err.message });
+//     });
+// });
+
+// module.exports = router;
