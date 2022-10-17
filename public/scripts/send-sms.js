79d20df8ac6edbc177d0693055895d966bcf6006
Matthew Davis <matc.davis@gmail.com>
2022-10-17 22:52:12 +0000

    more testing for twilio

--- public/scripts/send-sms.js
+++ public/scripts/send-sms.js
@@ -1,5 +1,6 @@
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+require('dotenv').config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
 
 const client = require('twilio')(accountSid, authToken);
 
