01f70eaea09509a5c5909bee29fcba7d8fb3859d
Sszyh <zhuyer@outlook.com>
2022-10-19 15:17:59 -0700

    fix the formatting problem of converting timestamp to date

--- public/scripts/helpers/getDateAndTime.js
+++ public/scripts/helpers/getDateAndTime.js
@@ -1,12 +1,21 @@
-// helper function to convert timestamp to date
+// helper function to convert timestamp to date yyyy-mm-dd 00:00:00
 export default function getDateAndTime  (timeStamp) {
+
+  function paddingZero (num) {
+    if(parseInt(num) < 10) {
+      num = '0' + num;
+    }
+    return num;
+  }
+
   const wholeData = new Date(parseInt(timeStamp));
   const year = wholeData.getFullYear();
-  const month = wholeData.getMonth();
-  const date = wholeData.getDate();
-  const hours = wholeData.getHours();
-  const minutes = wholeData.getMinutes();
-  const seconds = wholeData.getSeconds();
+  const month = paddingZero(wholeData.getMonth());
+  const date = paddingZero(wholeData.getDate());
+  const hours = paddingZero(wholeData.getHours());
+  const minutes = paddingZero(wholeData.getMinutes());
+  const seconds = paddingZero(wholeData.getSeconds());
 
   return year + "-" +month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+
 };
